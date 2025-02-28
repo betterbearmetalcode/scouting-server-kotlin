8fb7837a60c0aa060e2f5c2c386e2db03dc1a0f6
PiTBody <mykhailo.papenko@gmail.com>
2025-02-27 17:00:19 -0800

    Added the team ranking methods. Need further revising.

---
+++ src/main/kotlin/SDScoring/TeamScorer.kt
@@ -0,0 +1,17 @@
+package SDScoring
+
+import java.util.*
+
+fun teamScorer(weightedScoringMatrix: Array<DoubleArray>): DoubleArray {
+    val teamScores = DoubleArray(weightedScoringMatrix[0].size)
+    for (c in weightedScoringMatrix[0].indices) {
+        for (r in weightedScoringMatrix.indices) {
+            teamScores[c] += weightedScoringMatrix[r][c]
+        }
+    }
+    return teamScores
+}
+
+fun teamsRanked(teamScores: DoubleArray): DoubleArray {
+    return Arrays.stream(teamScores).sorted().toArray()
+}
