f5aa1ab88f2c8a8bfc849196272198c4572774f6
Jack J <sailboat31@outlook.com>
2025-03-31 16:22:26 -0700

    Undo testing database

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -46,7 +46,7 @@ fun App() {
     }
 }
 
-val manager = DatabaseManager(2025, true)
+val manager = DatabaseManager(2025)
 var server: Server? = null
 val chartValues = arrayOf(3,5,2,6,3,75)
 val chartColors = arrayOf(Color.Red, Color.Green, Color.Blue, Color.White)
